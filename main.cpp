d70aa34ba469da6bb50fcf2186d7f433351af0b1
Max Weidauer <max+git@weidauer.art>
2025-04-14 19:29:51 +0200

    made target addr configurable via environment var

--- main.cpp
+++ main.cpp
@@ -125,15 +125,21 @@ int main(){
     ))
 
     std::cout << "serving on " << server.getHTTPAddress() << std::endl;
-    
-    // TODO make wiebview addr configurable from command line for debug mode with external website
-    std::string webview_addr = "http://127.0.0.1:" +  std::to_string(server.getPort());
+
+    std::string target_addr = "127.0.0.1";
+
+    // overwrite target addr with environment var if necessary
+    char* target_addr_pointer = std::getenv("XENAX_WEBVIEW_TARGET");
+    if (target_addr_pointer != nullptr) target_addr = target_addr_pointer;
+
+    std::string webview_addr = "http://" + target_addr +  ":" +  std::to_string(server.getPort());
 
     std::cout << "pointing webview to " << webview_addr << std::endl;
     CHOC_ASSERT( webview.navigate( webview_addr ));
 
 
     window.toFront();
+    
 
     // std::cout << "Starting hellos from server" << std::endl;
 
